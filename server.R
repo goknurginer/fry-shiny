6130e98d8821361d6d2aba0b7b8d005be09f833c
goknurginer <goknurginer@gmail.com>
2016-04-27 09:03:25 +1000

    Initial commit

---
+++ server.R
@@ -0,0 +1,39 @@
+# server
+source("160418-geneset-fry.R")
+source("160414lmEffects.R")
+library(limma)
+library(org.Hs.eg.db)
+library(GO.db)
+.matvec <- limma:::.matvec
+goterms <- Term(GOTERM)
+gosets <- as.vector(goterms)
+shinyServer(function(input,output,session) {
+  updateSelectizeInput(session, "goSets", choices = gosets, server = TRUE)
+  output$countMatrix <- renderTable({
+    inFile <- input$counts
+    cat(file=stderr(), "drawing histogram with")
+    if (is.null(inFile)) return(NULL)
+    read.table(inFile$name)
+  })
+  
+  output$designMatrix <- renderTable({
+    inFile <- input$design
+    if (is.null(inFile)) return(NULL)
+    read.table(inFile$name)
+  })
+  
+  output$geneSetInput <- renderText({
+    if (is.null(input$goSets)) return(NULL)
+    print(input$goSets)
+  })
+  
+  output$fryTable <- renderTable({
+    if (input$run == TRUE) {
+      print(input$counts)
+      cnt <- read.table(input$counts$name)
+      dta <- read.table(input$design$name)
+      fry( cnt, design =dta, index = 1:5)
+    }
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,33 @@
+# ui
+# gosets <- as.vector(Term(GOTERM))
+keggsets <- paste("s", 1:100)
+shinyUI(fluidPage(
+  
+  titlePanel(h1("Fry: Fast Interactive Biological Pathway Miner")),
+  
+  sidebarLayout(position = "left",
+    sidebarPanel(h3("Please set up your test"),
+      fileInput("counts", label = "1. Upload the expression data matrix"),
+      fileInput("design", label = "2. Upload the design matrix"),
+      radioButtons("database", label = "3. Select a database",
+        choices = list("GO", "KEGG")),
+      p(strong("4. Input one or more gene sets")),
+      conditionalPanel(condition = "input.database == 'GO'", selectizeInput("goSets", label = "Search for gene sets within GO", choices = NULL, multiple = TRUE)),
+      conditionalPanel(condition = "input.database == 'KEGG'",selectizeInput("keggSets", label = "Search for gene sets within KEGG", choices = keggsets, multiple = TRUE)),
+      p("or"),
+      fileInput("geneList", label = "Upload the gene list of interest"),
+      p(strong("5. Decide default thresholds")),
+      selectInput("fdr", label = h5("FDR threshold"),
+        choices = list("0.01", "0.05", "0.1")),
+      selectInput("pvalue", label = h5("P-value threshold"),
+        choices = list("0.01", "0.05", "0.1")),
+      actionButton("run", label = strong("Apply gene set test"))
+    ),
+    mainPanel(
+      #tableOutput('countMatrix'),
+      #tableOutput('designMatrix'),
+      #tableOutput('fry'),
+      tableOutput("fryTable")
+    )
+  )
+))
