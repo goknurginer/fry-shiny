ddebd0bbe090fa0f53579a0d15fd19832b94094b
goknurginer <goknurginer@gmail.com>
2016-04-27 11:49:09 +1000

    foo replaced with goSets

--- ui.R
+++ ui.R
@@ -7,22 +7,39 @@ shinyUI(fluidPage(
   
   sidebarLayout(position = "left",
     sidebarPanel(h3("Please set up your test"),
+      
       fileInput("counts", label = "1. Upload the expression data matrix"),
+      
       fileInput("design", label = "2. Upload the design matrix"),
+      
       radioButtons("database", label = "3. Select a database",
         choices = list("GO", "KEGG")),
+      
       p(strong("4. Input one or more gene sets")),
-      conditionalPanel(condition = "input.database == 'GO'", selectizeInput("foo", label = "Search for gene sets within GO", choices = NULL, multiple = TRUE)),
-      conditionalPanel(condition = "input.database == 'KEGG'",selectizeInput("keggSets", label = "Search for gene sets within KEGG", choices = keggsets, multiple = TRUE)),
+      
+      conditionalPanel(condition = "input.database == 'GO'", 
+        selectizeInput("goSets", label = "Search for gene sets within GO", 
+          choices = NULL, multiple = TRUE)),
+      
+      conditionalPanel(condition = "input.database == 'KEGG'",
+        selectizeInput("keggSets", label = "Search for gene sets within KEGG",
+          choices = keggsets, multiple = TRUE)),
+      
       p("or"),
+      
       fileInput("geneList", label = "Upload the gene list of interest"),
+      
       p(strong("5. Decide default thresholds")),
+      
       selectInput("fdr", label = h5("FDR threshold"),
         choices = list("0.01", "0.05", "0.1")),
+      
       selectInput("pvalue", label = h5("P-value threshold"),
         choices = list("0.01", "0.05", "0.1")),
+      
       actionButton("run", label = strong("Apply gene set test"))
     ),
+    
     mainPanel(
       #tableOutput('countMatrix'),
       #tableOutput('designMatrix'),
